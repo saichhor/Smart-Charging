d9bb70ac68d2a30425ab8e86e3519bbaab3ac535
Stefan <sspahr@htl-steyr.ac.at>
2022-07-22 12:41:38 +0200

    httpsRequest.js added some authentication code now it works perfectly

--- communicationWithServerJAVAVersion/httpsRequest.js
+++ communicationWithServerJAVAVersion/httpsRequest.js
@@ -1,7 +1,20 @@
 var XMLHttpRequest = require('xhr2');
 var request = new XMLHttpRequest();
 
-request.open("GET","https://dns.loxonecloud.com/504F94A0EC9E/jdev/sps/io/Akkustand/75");
+var userName = "Experte";
+var passWord = "Experte1234";
+
+function authenticateUser(user, password)
+{
+    var token = user + ":" + password;
+
+    var hash = btoa(token);
+
+    return "Basic " + hash;
+}
+
+request.open("GET","https://dns.loxonecloud.com/504F94A0EC9E/jdev/sps/io/Akkustand/100");
+request.setRequestHeader("Authorization", authenticateUser(userName, passWord));
 request.addEventListener('load', function(event) {
     if (request.status >= 200 && request.status < 300) {
         console.log(request.responseText);
